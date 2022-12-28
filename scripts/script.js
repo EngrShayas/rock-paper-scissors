c31964269399c6a7014e906a0170268b84237394
Engr.Shayas <shayas.engr@gmail.com>
2022-12-28 18:54:11 +0530

    Created Function to return ComputerChoice

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,20 @@
 console.log('Rock paper Scissors');
+
+function getRandomNumber() {
+    return Math.floor(Math.random() * 3) + 1;
+}
+
+function getComputerChoice() {
+    let randomNumber = getRandomNumber();
+    if (randomNumber == 1) {
+        return 'ROCK';
+    } else if (randomNumber == 2) {
+        return 'PAPER';
+    } else if (randomNumber == 3) {
+        return 'SCISSORS';
+    } else {
+        return undefined;
+    }
+}
+
+console.log(getComputerChoice());
