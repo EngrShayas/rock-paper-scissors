159814716f23480be026163929122cffbbf2f872
Engr.Shayas <shayas.engr@gmail.com>
2023-01-02 08:07:50 +0530

    Added input by buttons and removed prompt input

--- scripts/script.js
+++ scripts/script.js
@@ -1,42 +1,31 @@
 console.log('Rock paper Scissors');
 
-function getRandomNumber() {
-    return Math.floor(Math.random() * 3) + 1;
+let playerScore = 0;
+let computerScore = 0;
+playRound();
+
+function playRound() {
+    let buttons = document.querySelectorAll('button');
+    buttons.forEach(button => {
+        button.addEventListener('click', () => {
+            let playerSelection = button.value;
+            let computerSelection = getComputerChoice();
+            play(playerSelection, computerSelection);
+        });
+    });
 }
 
 function getComputerChoice() {
-    let randomNumber = getRandomNumber();
-    if (randomNumber == 1) {
-        return 'ROCK';
-    } else if (randomNumber == 2) {
-        return 'PAPER';
-    } else if (randomNumber == 3) {
-        return 'SCISSORS';
-    } else {
-        return undefined;
-    }
+    let randomNumber = Math.floor(Math.random() * 3) + 1;
+    let choices = ['ROCK', 'PAPER', 'SCISSORS'];
+    return choices[randomNumber - 1];
 }
 
-function getPlayerChoice() {
-    let playerSelection = prompt(`Enter 'ROCK | PAPER | SCISSORS'`, '');
-    if (playerSelection == null || playerSelection == undefined) {
-        return;
-    }
-    else if (playerSelection.toUpperCase() === 'ROCK' || playerSelection.toUpperCase() === 'PAPER' || playerSelection.toUpperCase() === 'SCISSORS') {
-        return playerSelection.toUpperCase();
-    }
-    else {
-        return getPlayerChoice();
-    }
-}
-
-function play() {
-    let playerSelection = getPlayerChoice();
-    let computerSelection = getComputerChoice();
+function play(playerSelection, computerSelection) {
     if (playerSelection == undefined) {
         console.log('CANCEL');
     }
-    else if (playerSelection == computerSelection) {
+    if (playerSelection == computerSelection) {
         console.log(`Player Selected ${playerSelection} - ${playerScore}`);
         console.log(`Computer Selected ${computerSelection} - ${computerScore}`);
         playerScore += 0;
@@ -82,8 +71,7 @@ function play() {
 }
 
 
-let playerScore = 0;
-let computerScore = 0;
+
 // for (let i = 0; i < 5; i++) {
 //     play();
 // }
